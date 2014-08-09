92dd72372297d32c51deab5743ec6bf53c7a784d
Consys <kw3568@topmail-files.de>
2014-08-09 23:01:16 +0200

    Update

--- config.inc.php
+++ config.inc.php
@@ -4,11 +4,10 @@
  * @copyright	2014-2016 ConsysGroup
  * @license		ConsysGroup License
  */
-
 $board = [
 	'version' => '1.0.0 Alpha 1',
 	'versionCode' => 1000,
-	'language' => 'de',
+	'language' => 1,
 	'style' => 1,
 	'template' => 1,
 ];
