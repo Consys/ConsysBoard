11268344fb26267809a1e1086a044fe5a217ae87
Consys <kw3568@topmail-files.de>
2014-08-09 22:58:16 +0200

    Konfigurationsdatei

---
+++ config.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author		Philipp Bornemann
+ * @copyright	2014-2016 ConsysGroup
+ * @license		ConsysGroup License
+ */
+
+$board = [
+	'version' => '1.0.0 Alpha 1',
+	'versionCode' => 1000,
+	'language' => 'de',
+	'style' => 1,
+	'template' => 1,
+];
+?>
